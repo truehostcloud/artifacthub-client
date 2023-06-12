7413423a69e112f52d8defaccedd68e5e93db330
William Mwai <willymwaid92@gmail.com>
2023-06-12 10:34:43 +0300

    chore: rename directory

--- artifacthub_client/api/__init__.py
+++ artifacthub_client/api/__init__.py
@@ -3,12 +3,12 @@ from __future__ import absolute_import
 # flake8: noqa
 
 # import apis into api package
-from swagger_client.api.availability_checks_api import AvailabilityChecksApi
-from swagger_client.api.integrations_api import IntegrationsApi
-from swagger_client.api.organizations_api import OrganizationsApi
-from swagger_client.api.packages_api import PackagesApi
-from swagger_client.api.repositories_api import RepositoriesApi
-from swagger_client.api.stats_api import StatsApi
-from swagger_client.api.subscriptions_api import SubscriptionsApi
-from swagger_client.api.users_api import UsersApi
-from swagger_client.api.webhooks_api import WebhooksApi
+from artifacthub_client.api.availability_checks_api import AvailabilityChecksApi
+from artifacthub_client.api.integrations_api import IntegrationsApi
+from artifacthub_client.api.organizations_api import OrganizationsApi
+from artifacthub_client.api.packages_api import PackagesApi
+from artifacthub_client.api.repositories_api import RepositoriesApi
+from artifacthub_client.api.stats_api import StatsApi
+from artifacthub_client.api.subscriptions_api import SubscriptionsApi
+from artifacthub_client.api.users_api import UsersApi
+from artifacthub_client.api.webhooks_api import WebhooksApi
